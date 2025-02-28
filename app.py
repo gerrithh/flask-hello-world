d6f209d114753e1b92ba022c915b6989fd918eec
gerrithh <28559861+gerrithh@users.noreply.github.com>
2025-02-28 12:04:55 +0100

    added env variable

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask
+import os
 app = Flask(__name__)
 
+api_key = os.getenv('LLM_API_KEY')
+# print(api_key)
 @app.route('/')
 def hello_world():
-    return 'Hello, World!..,-'
+    return f'Hello, World!..,{api_key}'
