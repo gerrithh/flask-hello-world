9f8a4fc377a8c779fdde652aec18bd6a064fc3e9
gerrithh <28559861+gerrithh@users.noreply.github.com>
2025-02-28 13:56:00 +0100

    added MongoDB backend

--- app.py
+++ app.py
@@ -1,9 +1,38 @@
-from flask import Flask
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+from flask import Flask, render_template, request
 import os
+
 app = Flask(__name__)
 
 api_key = os.getenv('LLM_API_KEY')
-# print(api_key)
-@app.route('/')
+uri = os.getenv('MONGO_URI')
+
+
+# Create a new client and connect to the server
+client = MongoClient(uri, server_api=ServerApi('1'))
+# Send a ping to confirm a successful connection
+mydb = client["data"]
+mycol = mydb["FAQ"]
+
+
+@app.route('/',methods = ['POST', 'GET'])
 def hello_world():
-    return f'Hello, World!..,{api_key}'
+    context = {
+        'name': 'test',
+        'age': '35'
+    }
+    if request.method == 'POST':
+        result = request.form.to_dict()
+        context = {
+            'name': result['Key'],
+            'age': '35'
+        }
+        mydict = {"name": result['Key'], "address": "Highway 37"}
+        x = mycol.insert_one(mydict)
+        print(x)
+    return render_template('hello_world.html', **context)
+
+@app.route('/chart')
+def chart():
+    return render_template('chart.html')
